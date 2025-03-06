482df3e23b70fb7c1ee04f88db761413fb770fc7
mosszion <musdzion@gmail.com>
2025-03-06 06:25:03 -0500

    more example of methods in object

--- eloquent.js
+++ eloquent.js
@@ -707,3 +707,23 @@ rabbit.speak = function(line) {
 }
 
 rabbit.speak("I'm alive.")
+
+function speak(line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+};
+
+let whiteRabbit = {type:"white", speak} ;
+let hungryRabbit = {type:"hungry", speak} ;
+
+whiteRabbit.speak("oh my ears and whiskers, " + "how late its getting!");
+hungryRabbit.speak("I can use a carrot right now");
+
+speak.call(hungryRabbit, "Burp!")
+
+//////////////////////////////////////////////////////////////////////////////////
+
+function normalize() {
+  console.log(this.coords.map(n => n / this.length));
+}
+
+/////////////////////////////////////////////////////////////////
